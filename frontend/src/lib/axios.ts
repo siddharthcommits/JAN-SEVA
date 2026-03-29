cbc4d8bd60bd28a4268f019fe0fd75ec43046e60
Swastik <swastikiscoding@gmail.com>
2026-03-29 19:49:51 +0530

    feat: initialize axios instance with auth token interceptor and global 401 handling

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -3,7 +3,7 @@ import { useAuthStore } from '../store/authStore';
 
 export const api = axios.create({
 //  baseURL: 'http://localhost:3000', // Adjust to match your backend port
- baseURL: 'https://jan-seva-nine.vercel.app', // Adjust to match your backend port
+ baseURL: 'https://jan-seva-backend-phi.vercel.app', // Adjust to match your backend port
  headers: {
  'Content-Type': 'application/json',
  },
